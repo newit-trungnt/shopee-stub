57290ab00298eefd6e6f4bd963929ecc5b9bb822
newit-trungnt <trungnt@newit.co.jp>
2022-02-24 14:21:38 +0700

    feat: stub orders api

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
-const Koa = require("koa")
+const Koa = require('koa')
+const router = require('koa-router')()
+const logger = require('koa-logger')
+
+// import data
+const orderList = require('./data/orders/orders-list.json')
+const orderDetails = require('./data/orders/order-details.json')
+
 const app = new Koa()
 
-app.use(async (ctx) => {
-  ctx.body = "Hello"
+app.use(logger())
+
+router.get('/status', (ctx) => (ctx.body = 'OK'))
+
+router.post('/api/v1/orders/basics', function (ctx) {
+  ctx.body = orderList
 })
 
-app.listen(3000, () => console.log("App is running on port 3000"))
+router.post('/api/v1/orders/detail', function(ctx) {
+	ctx.body = orderDetails
+})
+
+
+app.use(router.routes())
+
+app.listen(3000, () => console.log('App is running on port 3000'))
